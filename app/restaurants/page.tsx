f9c619bbbddc8d1f279f8d997288c262f487979d
Shayan <shayanakhlaghian@gmail.com>
2023-10-17 01:24:20 +0330

    add cards and filter to restaurant page

--- app/restaurants/page.tsx
+++ app/restaurants/page.tsx
@@ -1,9 +1,25 @@
+import { RESTAURANTS } from '@/data/dummy-data';
 import Hero from './components/Hero';
+import Card from './components/Card';
+import Filter from './components/Filter';
 
 const Restaurants = () => {
   return (
     <>
       <Hero />
+      <div className='w-5/6 md:w-2/3 mx-auto mt-10 mb-20 px-4 grid grid-cols-restaurants justify-center gap-x-4 gap-y-8'>
+        {RESTAURANTS.map(({ id, image, name, cuisine, rating, reviews }) => (
+          <Card
+            key={id}
+            image={image}
+            name={name}
+            cuisine={cuisine}
+            rating={rating}
+            reviews={reviews}
+          />
+        ))}
+      </div>
+      <Filter />
     </>
   );
 };
