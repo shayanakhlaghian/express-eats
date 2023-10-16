c2df7e901e9c17d32612c63243636b54b7300c74
Shayan <shayanakhlaghian@gmail.com>
2023-10-16 16:59:25 +0330

    add disabled prop and related styles

--- components/SecondaryButton.tsx
+++ components/SecondaryButton.tsx
@@ -1,14 +1,17 @@
 const SecondaryButton = ({
   children,
   onClick,
+  disabled = false,
 }: {
   children: React.ReactNode;
   onClick?: () => void;
+  disabled?: boolean;
 }) => {
   return (
     <button
       onClick={onClick}
-      className='bg-transparent border-2 border-primary text-primary capitalize font-bold tracking-wide px-4 py-1 md:px-5 lg:px-6 rounded-full hover:bg-primary hover:text-white text-sm md:text-base duration-100'
+      disabled={disabled}
+      className='bg-transparent border-2 border-primary text-primary capitalize font-bold tracking-wide px-4 py-1 md:px-5 lg:px-6 rounded-full hover:bg-primary hover:text-white text-sm md:text-base duration-100 disabled:border-gray-400 disabled:text-gray-400 disabled:hover:bg-transparent disabled:cursor-not-allowed'
     >
       {children}
     </button>
