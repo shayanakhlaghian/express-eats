140919f3bd5f8cc55ab12076be98859e248a34ee
Shayan <shayanakhlaghian@gmail.com>
2023-10-16 16:31:14 +0330

    change paddings on primary button

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -8,7 +8,7 @@ const PrimaryButton = ({
   return (
     <button
       onClick={onClick}
-      className='bg-primary text-white capitalize font-bold tracking-wide px-4 py-1 md:px-5 lg:px-6 rounded-full hover:bg-primary-dark text-sm md:text-base duration-100 border-2 border-primary hover:border-primary-dark'
+      className='bg-primary text-white capitalize font-bold tracking-wide px-4 py-1 md:py-2 md:px-6 lg:px-8 rounded-full hover:bg-primary-dark text-sm md:text-base duration-100 border-2 border-primary hover:border-primary-dark'
     >
       {children}
     </button>
