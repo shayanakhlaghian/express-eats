9ed9980493d85091014b5947668be7c126c271f5
Shayan <shayanakhlaghian@gmail.com>
2023-10-17 01:06:03 +0330

    change heading z index

--- app/restaurants/components/Card.tsx
+++ app/restaurants/components/Card.tsx
@@ -19,7 +19,7 @@ const Card = ({
         <Image src={image} alt={name} fill className='object-cover' />
       </div>
       <div className='flex justify-center items-center'>
-        <h3 className='bg-gradient-to-r from-primary-light to-primary-dark text-white w-1/2 rounded-sm text-center mt-[-2rem] relative z-10 px-1 py-2 font-bold tracking-wide line-clamp-1'>
+        <h3 className='bg-gradient-to-r from-primary-light to-primary-dark text-white w-1/2 rounded-sm text-center mt-[-2rem] relative z-[5] px-1 py-2 font-bold tracking-wide line-clamp-1'>
           {name}
         </h3>
       </div>
