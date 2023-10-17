2a6f6f3b22fe7a35c77af9ad0d752f78bf56a4f2
Shayan <shayanakhlaghian@gmail.com>
2023-10-17 15:11:53 +0330

    add id and link to restaurant cards in restaurants page

--- app/restaurants/components/Card.tsx
+++ app/restaurants/components/Card.tsx
@@ -1,12 +1,15 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Card = ({
+  id,
   image,
   name,
   cuisine,
   rating,
   reviews,
 }: {
+  id: string;
   image: string;
   name: string;
   cuisine: string;
@@ -14,23 +17,25 @@ const Card = ({
   reviews: number;
 }) => {
   return (
-    <div className='overflow-hidden h-72 rounded-lg border border-gray-200 shadow-lg grid grid-rows-8 cursor-pointer hover:shadow-xl duration-200'>
-      <div className='row-start-1 row-end-5 relative w-full h-full'>
-        <Image src={image} alt={name} fill className='object-cover' />
+    <Link href={`/restaurants/${id}`} className='w-full h-full'>
+      <div className='overflow-hidden h-72 rounded-lg border border-gray-200 shadow-lg grid grid-rows-8 cursor-pointer hover:shadow-xl duration-200'>
+        <div className='row-start-1 row-end-5 relative w-full h-full'>
+          <Image src={image} alt={name} fill className='object-cover' />
+        </div>
+        <div className='flex justify-center items-center'>
+          <h3 className='bg-gradient-to-r from-primary-light to-primary-dark text-white w-1/2 rounded-sm text-center mt-[-2rem] relative z-[5] px-1 py-2 font-bold tracking-wide line-clamp-1'>
+            {name}
+          </h3>
+        </div>
+        <div className='flex justify-center items-center'>
+          <p className='capitalize'>{cuisine} cuisine</p>
+        </div>
+        <div className='flex items-center px-4 font-bold justify-around'>
+          <p>{rating}/10</p>
+          <p>{reviews} reviews</p>
+        </div>
       </div>
-      <div className='flex justify-center items-center'>
-        <h3 className='bg-gradient-to-r from-primary-light to-primary-dark text-white w-1/2 rounded-sm text-center mt-[-2rem] relative z-[5] px-1 py-2 font-bold tracking-wide line-clamp-1'>
-          {name}
-        </h3>
-      </div>
-      <div className='flex justify-center items-center'>
-        <p className='capitalize'>{cuisine} cuisine</p>
-      </div>
-      <div className='flex items-center px-4 font-bold justify-around'>
-        <p>{rating}/10</p>
-        <p>{reviews} reviews</p>
-      </div>
-    </div>
+    </Link>
   );
 };
 export default Card;
