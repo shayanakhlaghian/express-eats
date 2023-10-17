c293cfc9364abef0060aba63bbd2472cc072d47c
Shayan <shayanakhlaghian@gmail.com>
2023-10-17 16:50:12 +0330

    do minor styling fixes in filter select

--- app/restaurants/components/FilterSelect.tsx
+++ app/restaurants/components/FilterSelect.tsx
@@ -6,9 +6,13 @@ const FilterSelect = ({
   name: string;
 }) => {
   return (
-    <div className='flex flex-col items-center border-b border-gray-700 py-4 w-2/3'>
+    <div className='flex flex-col items-center border-b border-gray-700  pb-2 w-[60%] md:w-1/2'>
       <h4 className='font-bold mb-2 capitalize'>{name}</h4>
-      <select name={name} id={name} className='px-8 py-1 rounded-md'>
+      <select
+        name={name}
+        id={name}
+        className='px-8 py-1 rounded-md font-sans bg-transparent text-center cursor-pointer'
+      >
         {children}
       </select>
     </div>
