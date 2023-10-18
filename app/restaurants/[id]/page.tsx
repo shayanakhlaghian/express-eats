2bd00d03d8bb9bb7cd35fd87229d70d696d25e73
Shayan <shayanakhlaghian@gmail.com>
2023-10-18 14:03:52 +0330

    mimic network call in restaurants pages

--- app/restaurants/[id]/page.tsx
+++ app/restaurants/[id]/page.tsx
@@ -1,3 +1,6 @@
+import { notFound } from 'next/navigation';
+
+import delay from '@/lib/delay';
 import { RESTAURANTS } from '@/data/dummy-data';
 import BookingCard from './components/BookingCard';
 import RestaurantDetail from './components/RestaurantDetail';
@@ -5,17 +8,17 @@ import RestaurantDetail from './components/RestaurantDetail';
 const getRestaurant = (restaurantId: string) =>
   RESTAURANTS.find(({ id }) => id === restaurantId);
 
-const RestaurangeDetail = ({ params }: { params: { id: string } }) => {
-  const restaurant = getRestaurant(params.id);
+const RestaurangeDetail = async ({ params }: { params: { id: string } }) => {
+  const data = await delay(2000, () => getRestaurant(params.id));
 
-  if (!restaurant) return <div>Not found</div>;
+  if (!data) return notFound();
   return (
     <div className='w-[90%] md:w-[80%] lg:w-2/3 mx-auto grid grid-rows-2 md:grid-cols-[2fr_1fr] my-20 md:my-24 gap-y-4 md:gap-y-4 md:gap-x-2'>
       <RestaurantDetail
-        image={restaurant.image}
-        name={restaurant.name}
-        rating={restaurant.rating}
-        reviews={restaurant.reviews}
+        image={data.image}
+        name={data.name}
+        rating={data.rating}
+        reviews={data.reviews}
       />
       <div>
         <BookingCard />
--- app/restaurants/page.tsx
+++ app/restaurants/page.tsx
@@ -1,14 +1,23 @@
 import { RESTAURANTS } from '@/data/dummy-data';
+import delay from '@/lib/delay';
 import Hero from './components/Hero';
 import Card from './components/Card';
 import Filter from './components/Filter';
+import PrimaryHeading from '@/components/PrimaryHeading';
 
-const Restaurants = () => {
+const Restaurants = async () => {
+  const data = await delay(2000, () => RESTAURANTS);
+
   return (
     <>
       <Hero />
       <div className='w-5/6 md:w-2/3 mx-auto mt-10 mb-20 px-4 grid grid-cols-restaurants justify-center gap-x-4 gap-y-8'>
-        {RESTAURANTS.map(({ id, image, name, cuisine, rating, reviews }) => (
+        {data && data.length === 0 && (
+          <div className='mt-20'>
+            <PrimaryHeading>No restaurants found!</PrimaryHeading>
+          </div>
+        )}
+        {data?.map(({ id, image, name, cuisine, rating, reviews }) => (
           <Card
             key={id}
             id={id}
