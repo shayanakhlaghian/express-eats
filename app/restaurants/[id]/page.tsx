c5e5de71180d1e849738e5106a9c30e017f5733b
Shayan <shayanakhlaghian@gmail.com>
2023-10-19 13:34:39 +0330

    do minor styling fixed to restaurant detail

--- app/restaurants/[id]/page.tsx
+++ app/restaurants/[id]/page.tsx
@@ -13,7 +13,7 @@ const RestaurangeDetail = async ({ params }: { params: { id: string } }) => {
 
   if (!data) return notFound();
   return (
-    <div className='w-[90%] md:w-[80%] lg:w-2/3 mx-auto grid grid-rows-2 md:grid-rows-1 md:grid-cols-[2fr_1fr] my-20 md:my-24 gap-y-4 md:gap-y-4 md:gap-x-2'>
+    <div className='w-[90%] md:w-[80%] lg:w-2/3 mx-auto grid grid-rows-[1fr_auto] md:grid-rows-1 md:grid-cols-[2fr_1fr] my-20 md:my-24 gap-y-4 md:gap-y-4 md:gap-x-2'>
       <RestaurantDetail
         image={data.image}
         name={data.name}
