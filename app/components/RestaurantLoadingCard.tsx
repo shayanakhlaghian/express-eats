3f297b945dc0b9f5b536a8cbafc5c8538ff59915
Shayan <shayanakhlaghian@gmail.com>
2023-10-19 14:29:30 +0330

    create restaurant loading card

---
+++ app/components/RestaurantLoadingCard.tsx
@@ -0,0 +1,16 @@
+import { ShimmerThumbnail } from 'react-shimmer-effects';
+
+const RestaurantLoadingCard = () => {
+  return (
+    <div className='h-72 overflow-hidden rounded-xl border-2 border-gray-100 grid gap-y-1 grid-rows-[3fr_1fr_1fr] group cursor-pointer w-52'>
+      <div className='relative w-full h-full grayscale group-hover:grayscale-0 duration-200'>
+        <ShimmerThumbnail height={100} />
+      </div>
+      <div className='flex items-center flex-col'>
+        <ShimmerThumbnail height={20} width={100} />
+        <ShimmerThumbnail height={20} width={80} />
+      </div>
+    </div>
+  );
+};
+export default RestaurantLoadingCard;
