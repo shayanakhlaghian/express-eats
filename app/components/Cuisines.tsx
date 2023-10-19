b57c07c863cd65fb4ebd89bd26d7524ce043ea5a
Shayan <shayanakhlaghian@gmail.com>
2023-10-19 14:08:49 +0330

    add cuisine loading card to cuisines

--- app/components/Cuisines.tsx
+++ app/components/Cuisines.tsx
@@ -1,14 +1,28 @@
+'use client';
+import { useQuery } from '@tanstack/react-query';
+
+import delay from '@/lib/delay';
 import { CUISINES } from '@/data/dummy-data';
 import Container from '@/components/Container';
 import PrimaryHeading from '@/components/PrimaryHeading';
 import CuisineCard from './CuisineCard';
+import CuisineLoadingCard from './CuisineLoadingCard';
 
 const Cuisines = () => {
+  const cuisinesQuery = useQuery({
+    queryKey: ['cuisines'],
+    queryFn: () => delay(5000, () => CUISINES),
+  });
+
   return (
     <Container>
       <PrimaryHeading>Cuisines</PrimaryHeading>
       <ul className='grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-x-8 lg:gap-x-12'>
-        {CUISINES.map(({ id, image, name }) => (
+        {cuisinesQuery.isLoading &&
+          Array.from(Array(6).keys()).map((_, i) => (
+            <CuisineLoadingCard key={i} />
+          ))}
+        {cuisinesQuery.data?.map(({ id, image, name }) => (
           <CuisineCard key={id} image={image} name={name} />
         ))}
       </ul>
