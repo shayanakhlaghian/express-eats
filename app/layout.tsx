8812e30352f60540eb54b9b02deb7ac1df086eba
Shayan <shayanakhlaghian@gmail.com>
2023-10-19 13:50:58 +0330

    add providers to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Lato } from 'next/font/google';
 
+import Providers from './providers';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
@@ -23,9 +24,11 @@ export default function RootLayout({
   return (
     <html lang='en' className='overflow-x-hidden'>
       <body className={`${lato.className} overflow-x-hidden`}>
-        <Header />
-        <main className='min-h-screen w-screen'>{children}</main>
-        <Footer />
+        <Providers>
+          <Header />
+          <main className='min-h-screen w-screen'>{children}</main>
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
